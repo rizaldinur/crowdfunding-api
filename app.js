3fcd307a55def9315c313331067a0d6bf64d1c12
rizaldinur <rnur71@gmail.com>
2025-05-03 15:38:01 +0700

    init app serve

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.listen(3000, () => {
+  console.log("Server started: http://localhost:3000");
+});
