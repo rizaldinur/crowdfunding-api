6a4f5f0f9d8bc98000b9c47494664825869e01b1
rizaldinur <rnur71@gmail.com>
2025-05-07 00:57:43 +0700

    create standalone authenticate guard route

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ import * as authController from "../controllers/auth.js";
 const router = Router();
 
 router.post("/login", authController.login);
-
 router.post("/signup", authController.signup);
+router.post("/authenticate", authController.authJWT);
 
 export default router;
