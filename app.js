c7d067c1ba3fdcacf72042eb9f6ff54c6420b2da
rizaldinur <rnur71@gmail.com>
2025-06-16 21:14:33 +0700

    handle allowed multiple origins

--- app.js
+++ app.js
@@ -31,7 +31,16 @@ app.use("/images", express.static("images"));
 app.use("/data", express.static("data"));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_ORIGIN_URL);
+  const allowedOrigins = [
+    process.env.CLIENT_ORIGIN_URL,
+    process.env.PREVIEW_CLIENT_ORIGIN_URL,
+  ];
+
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin); // ✅ Dynamically allow only matched origin
+  }
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") {
